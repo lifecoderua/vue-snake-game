bf3e9ec0f606b7139e8a203c7ff9a376502b3e25
lifecoder <lifecoder.ua@gmail.com>
2021-01-03 13:26:16 +0200

    Snake game init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Game msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import Game from './components/Game.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    Game,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/Game.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>A little snake game</h1>
+    <p>
+      A little red hood snake game I am.
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+  }
+})
+export default class Game extends Vue {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
